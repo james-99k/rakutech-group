532dd4d53cf2aa25cce0b591c6671ccf8dbc84ab
james-99k <jamesgasty@gmail.com>
2021-08-20 14:06:33 +0800

    react login and register UI

---
+++ REACT/src/components/login.component.js
@@ -0,0 +1,186 @@
+import React, { Component } from "react";
+import { Redirect, Link } from 'react-router-dom';
+import './login.component.scss'
+
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { connect } from "react-redux";
+import { login } from "../actions/auth";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = {
+      username: "",
+      password: "",
+      loading: false,
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
+    this.form.validateAll();
+
+    const { dispatch, history } = this.props;
+
+    if (this.checkBtn.context._errors.length === 0) {
+      dispatch(login(this.state.username, this.state.password))
+        .then(() => {
+          history.push("/profile");
+          window.location.reload();
+        })
+        .catch(() => {
+          this.setState({
+            loading: false
+          });
+        });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { isLoggedIn, message } = this.props;
+
+    if (isLoggedIn) {
+      return <Redirect to="/profile" />;
+    }
+
+    return (
+      <div className="containerBox">
+
+        <div className="containerLeft">
+          <div className="companyLogo">
+            <Link to={"/"}><a><h1 className="white">RAKU</h1><h1>TECH</h1></a></Link>
+          </div>
+          <p>Browse from over 500 devices in your area.</p>
+        </div>
+
+        <div className="containerRight">
+          <div className="title">
+            <h1>Sign In</h1>
+          </div>
+          <div className="loginBtn">
+            <button className="login green">
+              <div className="logo">
+                <h1>G</h1>
+              </div>Sign in with Google
+            </button>
+            <button className="login blue">
+              <div className="logo">
+                <h1>F</h1>
+              </div>Continue with Facebook</button>
+          </div>
+          <div className="divider">
+            <p></p>
+          </div>
+          <Form
+            onSubmit={this.handleLogin}
+            ref={(c) => {
+              this.form = c;
+            }}
+          >
+            <div className="input">
+              <label htmlFor="username">Username</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="username"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                validations={[required]}
+              />
+            </div>
+
+            <div className="input">
+              <label htmlFor="password">Password</label>
+              <Input
+                type="password"
+                className="form-control"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChangePassword}
+                validations={[required]}
+              />
+            </div>
+
+            <div className="signIn">
+              <button
+                className="signInBtn"
+                disabled={this.state.loading}
+              >
+                {this.state.loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Sign In</span>
+              </button>
+            </div>
+            <p className="terms_agreement">By continuing, you agree to accept our Privacy Policy & Terms of Service.</p>
+
+            <p className="registration">Don't have an account? <Link to={"register"}><a >Create new account</a></Link></p>
+
+            {message && (
+              <div className="form-group">
+                <div className="alert alert-danger" role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              style={{ display: "none" }}
+              ref={(c) => {
+                this.checkBtn = c;
+              }}
+            />
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { isLoggedIn } = state.auth;
+  const { message } = state.message;
+  return {
+    isLoggedIn,
+    message
+  };
+}
+
+export default connect(mapStateToProps)(Login);
